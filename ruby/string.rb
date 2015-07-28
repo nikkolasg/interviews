a7d9515ea75d8f58a0ba98f8dc95f07137b4742e
nicolas gailly <gailly@ned-team.com>
2015-07-28 16:47:47 +0200

    non repeated

--- ruby/string.rb
+++ ruby/string.rb
@@ -1,7 +1,16 @@
 def first_nonrepeated string
-
+    h = Hash.new { |h,k| h[k] = [] }
+    string.split("").each_with_index  { |c,i| h[c] << i }
+    fchar,index = nil,string.size
+    h.each do |char,indexes|
+        next if indexes.size != 1
+        fchar,index = char,indexes.first if indexes.first < index
+    end
+    return fchar
 end
 
 puts "[+] String problems "
+str = "tweeter"
+puts "[+] first non repeated char in #{str} : #{first_nonrepeated(str)}"
 
 
